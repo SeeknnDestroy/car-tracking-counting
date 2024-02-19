473369c95db821d8f2e2c63efb991311354d84c1
SeeknnDestroy <sarit17@itu.edu.tr>
2024-02-19 17:08:53 +0300

    add config

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# Configuration for line positions and frame dimensions
+line_position_horizontal: int = 800
+line_position_perpendicular: int = 1200
+frame_width: int = 1920
+frame_height: int = 1080
+
+# Configuration for the model and inference
+device: str = 'cuda:0'  # Use 'cpu' for CPU
+imgsz: tuple[int, int] = (384, 640)
+model_path: str = 'yolov8m.pt'
